15b298520aa609b520d65318a8991f50f20c5d30
CHRIS <NWAKAUC1@GMAIL.COM>
2023-06-01 15:15:42 +0200

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
 
-  authenticated :user do 
+  authenticated :user do
     root 'categories#index', as: :authenticated_root
   end
 
-  unauthenticated do 
+  unauthenticated do
     root 'public#splash', as: :unauthenticated_root
   end
 
-  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy, :show] do 
-    resources :expenses, only: [:new, :create, :destroy] 
+  resources :categories, only: %i[index new create edit update destroy show] do
+    resources :expenses, only: %i[new create destroy]
   end
-
 end
